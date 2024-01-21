2316d12c699d0e175acda24003c3f794d6bdd651
fagnersouza666 <fagnersouza@gmail.com>
2024-01-20 23:46:14 -0300

    ajustes

--- settings.py
+++ settings.py
@@ -2,34 +2,36 @@ from core.domain.distribution import Distribution, EqualDistribution, CustomDist
 
 
 crypto_assets = [
-    'BTC',
-    'ETH',
-    'XAI',
-    'SEI',
-    'SOL',
-    'MATIC',
-    'PEPE',
-    'DOGE',
-    'BNB',
-    'LDO'
+    "BTC",
+    "ETH",
+    "XAI",
+    "SEI",
+    "SOL",
+    "MATIC",
+    "PEPE",
+    "DOGE",
+    "BNB",
+    "LDO",
+    "PENDLE",
 ]
 
-fiat_asset = 'USDT'
+fiat_asset = "USDT"
 fiat_decimals = 2
-fiat_untouched = float(len(crypto_assets) * 6)
+fiat_untouched = float(len(crypto_assets) * 0)
 exposure = 1.0  # max 1.0, min 0.0
 distribution = CustomDistribution(
     crypto_assets=crypto_assets,
     percentages={
-        'BTC': 25,
-        'ETH': 20,
-        'XAI': 10,
-        'SEI': 10,
-        'SOL': 15,
-        'MATIC': 10,
-        'PEPE': 1,
-        'DOGE': 1,
-        'BNB': 3,
-        'LDO': 5
-    }
+        "BTC": 25,
+        "ETH": 20,
+        "XAI": 10,
+        "SEI": 10,
+        "SOL": 15,
+        "MATIC": 10,
+        "PEPE": 1,
+        "DOGE": 1,
+        "BNB": 3,
+        "LDO": 4,
+        "PENDLE": 1,
+    },
 )
